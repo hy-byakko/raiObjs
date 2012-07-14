bfed6ddcfddfc795cb7eadd070f341c4976bd6a5
byakko <hy.byakko@gmail.com>
2012-07-14 09:27:19 +0800

    Create

---
+++ app/controllers/bumon_controller.rb
@@ -0,0 +1,5 @@
+class BumonController < ApplicationController
+  def index
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HomeWork::Application.config.secret_token = 'aeea9fbe589b8c420979aaf2123626178bc043f3e9cb05d9fc827a66363e89a82140fe0b2677609ca56fba57300642205401ee4ac3a0ff1a50b30d0962ff8476'
